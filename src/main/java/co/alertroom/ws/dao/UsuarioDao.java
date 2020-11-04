9502f024caf112accf436a3220538beca1e27d51
juanjortiz <jjortizsanta97@gmail.com>
2020-11-04 15:09:12 -0500

    Servicio usuario entregado por maicol y daniela

--- src/main/java/co/alertroom/ws/dao/UsuarioDao.java
+++ src/main/java/co/alertroom/ws/dao/UsuarioDao.java
@@ -1,18 +1,83 @@
 package co.alertroom.ws.dao;
 
+
+import java.util.List;
+
 import co.alertroom.ws.adapter.UsuarioAdapter;
 import co.alertroom.ws.vo.UsuarioVo;
+import co.jjortiz.dao.UsuariosDAO;
 import co.jjortiz.entidades.Usuario;
+
 
 public class UsuarioDao {
 
 	public UsuarioVo consultarLoginUsuario(String id, String contrasena) {
-		co.jjortiz.dao.UsuariosDAO usuarioDaoJpa = new co.jjortiz.dao.UsuariosDAO();
+		
+		co.jjortiz.dao.UsuariosDAO usuarioDaoJpa= new UsuariosDAO();
 		Usuario usuarioJpa = usuarioDaoJpa.consultarLoginUsuario(id,contrasena);
 		UsuarioAdapter usuarioAdapter = new UsuarioAdapter();
 		UsuarioVo usuarioVo = usuarioAdapter.asignarUsuario(usuarioJpa);
 		usuarioDaoJpa.close();
 		return usuarioVo;
+	}
+
+	public String registrarUsuario(UsuarioVo usuarioVo) {
+		
+		co.jjortiz.dao.UsuariosDAO usuarioDaoJpa= new UsuariosDAO();
+		String mensaje="";
+		UsuarioAdapter usuarioAdapter = new UsuarioAdapter();
+		Usuario usuarioJpa = usuarioAdapter.obtenerUsuario(usuarioVo);
+		mensaje=usuarioDaoJpa.registrarUsuario(usuarioJpa);
+		
+		usuarioDaoJpa.close();
+		return mensaje;
+	}
+
+	public String actualizarUsuario(String documento, Usuario usuario) {
+		co.jjortiz.dao.UsuariosDAO usuarioDaoJpa= new UsuariosDAO();
+		String resp="";
+		if(usuarioDaoJpa.consultarUsuario(documento)!=null){
+			UsuarioAdapter usuarioAdapter = new UsuarioAdapter();
+			Usuario miUsuario = usuarioAdapter.actualizarUsuario(usuario);
+			resp=usuarioDaoJpa.actualizarUsuario(miUsuario);
+			
+		}
+		usuarioDaoJpa.close();
+		return resp;
+	}
+
+	public UsuarioVo consultarUsuario(String documento) {
+		co.jjortiz.dao.UsuariosDAO usuarioDaoJpa= new UsuariosDAO();
+		
+		Usuario usuarioJpa=usuarioDaoJpa.consultarUsuario(documento);
+		UsuarioAdapter usuarioAdapter = new UsuarioAdapter();
+		UsuarioVo usuarioVo = usuarioAdapter.asignarUsuario(usuarioJpa);
+	
+		usuarioDaoJpa.close();
+		return usuarioVo;
+	}
+	
+	
+
+	public UsuarioVo actualizarEstado(String documento) {
+		
+		co.jjortiz.dao.UsuariosDAO usuarioDaoJpa= new UsuariosDAO();
+		
+		Usuario usuarioJpa=usuarioDaoJpa.consultarUsuario(documento);
+		UsuarioAdapter usuarioAdapter = new UsuarioAdapter();
+		UsuarioVo usuarioVo=usuarioAdapter.actualizarEstado(usuarioJpa);
+		Usuario usuario=usuarioAdapter.obtenerUsuario(usuarioVo);
+		usuarioDaoJpa.actualizarUsuario(usuario);
+		usuarioDaoJpa.close();
+		return usuarioVo;
+		
+	}
+	
+	
+	public List<Usuario> listarUsuarios() {
+		co.jjortiz.dao.UsuariosDAO usuarioDaoJpa = new co.jjortiz.dao.UsuariosDAO();
+		usuarioDaoJpa.close();
+		return usuarioDaoJpa.listarUsuarios();
 	}
 
 }
--- src/main/java/co/alertroom/ws/rest/UsuariosService.java
+++ src/main/java/co/alertroom/ws/rest/UsuariosService.java
@@ -1,13 +1,22 @@
 package co.alertroom.ws.rest;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
 
 import co.alertroom.ws.dao.UsuarioDao;
 import co.alertroom.ws.vo.UsuarioVo;
+
+import co.jjortiz.entidades.Usuario;
 
 
 
@@ -16,13 +25,120 @@ public class UsuariosService {
 	
 	UsuarioDao miUsuarioDao = new UsuarioDao();
 
+	//http://localhost:8080/AlertRoomWebServices/api/usuarios/login
+	
+	
 	@POST
 	@Path("/login")
 	@Consumes({MediaType.APPLICATION_JSON})
 	@Produces({MediaType.APPLICATION_JSON})
-	public UsuarioVo validarUsuario(UsuarioVo usuarioVo) {
+	public Response validarUsuario(UsuarioVo usuarioVo) {
 		System.out.println("Estoy en validar usuario desde el servicio en el proyecto webservices");
-		return miUsuarioDao.consultarLoginUsuario(usuarioVo.getId(), usuarioVo.getContrasena());	
+		
+		UsuarioVo miUsuarioVo=miUsuarioDao.consultarLoginUsuario(usuarioVo.getId(), usuarioVo.getContrasena());
+		
+		try {
+			if (miUsuarioVo!=null) {
+				return Response.status(Response.Status.OK).entity(miUsuarioVo).build();
+			}
+			else {
+				return Response.status(Response.Status.NOT_FOUND).build();
+			}
+			//return miUsuarioDao.consultarLoginUsuario(usuarioVo.getId(), usuarioVo.getContrasena());
+		} catch (Exception e) {
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
+			
 	}
+	
+	
+
+	//http://localhost:8080/AlertRoomWebServices/api/usuarios/registrarPersona
+		
+	
+	@POST
+	@Path("/registrarPersona")
+	@Consumes({MediaType.APPLICATION_JSON})
+	//@Produces({MediaType.APPLICATION_JSON})
+	public Response registrarPersona(UsuarioVo usuarioVo) {
+		System.out.println("Registrar Usuario");
+		try {
+			String res = miUsuarioDao.registrarUsuario(usuarioVo);
+			if (res.equals("ok")) {
+				return Response.ok().build();
+				//return Response.status(Response.Status.OK).entity(persona).build();
+			}else {
+				return Response.status(Response.Status.NOT_FOUND).build();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+	
+	
+	
+	//http://localhost:8080/AlertRoomWebServices/api/usuarios/123
+	
+	@PUT
+	@Path("/{id}")
+	@Consumes({MediaType.APPLICATION_JSON})
+	@Produces({MediaType.APPLICATION_JSON})
+	public Response actualizarUsuario(@PathParam("id") String documento, Usuario usuario) {
+		try {
+			String resp=miUsuarioDao.actualizarUsuario(documento,usuario);
+			if(resp.equals("Persona Actualizada!")) {
+				return Response.ok().build();
+				//return Response.status(Response.Status.OK).entity(persona).build();
+			}else {
+				return Response.status(Response.Status.NOT_FOUND).build();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
+	}
+	
+	
+	
+	//http://localhost:8080/AlertRoomWebServices/api/usuarios/123
+	
+	@GET
+	@Path("{id}")
+	@Produces({ MediaType.APPLICATION_JSON })
+	public UsuarioVo getPersonaId(@PathParam("id") String documento) {
+		UsuarioVo persona = miUsuarioDao.consultarUsuario(documento);
+		if (persona == null) {
+			persona = new UsuarioVo();
+		}
+		return persona;
+	}
+	
+	
+	
+	//http://localhost:8080/AlertRoomWebServices/api/usuarios/actualizarEstado/123
+	
+	@PUT
+	@Path("/actualizarEstado/{id}")
+	public UsuarioVo actualizarEstado(@PathParam("id") String documento) {
+		UsuarioVo persona = miUsuarioDao.actualizarEstado(documento);
+		if (persona == null) {
+			persona = new UsuarioVo();
+		}
+		return persona;
+	}
+	
+	
+	//http://localhost:8080/AlertRoomWebServices/api/usuarios/listarUsuarios
+	
+	@GET
+	@Path("/listarUsuarios")
+	@Produces({MediaType.APPLICATION_JSON})
+	public List<Usuario> ListarUsuarios(){
+		List<Usuario> lista = miUsuarioDao.listarUsuarios();
+		return lista;
+	}
+	
+
 	
 }
